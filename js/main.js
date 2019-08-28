ac29d5ae3a0a1df396a2029502284cbd369fc7c6
Farzan Bhuiyan <51684369+Farzanb49@users.noreply.github.com>
2019-08-28 19:09:27 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  document.getElementById("particles-js").setAttribute("id", "mobile");
+  document.getElementById("execpics").setAttribute("class", "spacingm");
+}
